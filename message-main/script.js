14d231463d8714c495b10e8e9f54012405235eda
kwailan98 <152774341+kwailan98@users.noreply.github.com>
2026-05-06 12:17:48 +0800

    Update script.js

--- message-main/script.js
+++ message-main/script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Missing you too!!! <3";
+  question.innerHTML = "Missing you too! <3";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
